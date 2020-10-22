fc9bf1e95c6bbc76a79770c0eb4dc42550f01bdd
Victor Emanoel <victoremaanoel@outlook.com>
2020-10-22 01:19:58 -0300

    Primeiro Arquivo

---
+++ gg.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int a,b, result;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d",&a);
+    printf("\nDigite o segundo numero: ");
+
+    scanf("%d",&b);
+
+    result = a * b;
+
+    printf("\na multiplicacao deu %d\n", result);
+
+    printf("\nHello World!"); 
+    return 0;
+}
+
+
